36cea6663196b6571717dc14107a38a25703ab6b
David Pritchard <david.al.pritchard@gmail.com>
2022-06-16 17:49:47 -0400

    add floating-point-arith example

---
+++ floating-point-arith/floating-point-arith.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    double x = pow(9.0, 17.0);
+    double y = 9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0*9.0;
+
+    printf("x: %f\ny: %f\n", x, y);
+
+    return 0;
+}
